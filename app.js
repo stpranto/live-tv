acdbb264a26a502fe4e1327fa55e918e7bbc5454
Sha Taskin <138521847+stpranto@users.noreply.github.com>
2026-06-24 14:45:09 +0600

    Add streaming channels data to app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const channels = [
+  {
+    name: "NASA TV",
+    category: "Science",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg",
+    url: "https://nasa-i.akamaihd.net/hls/live/253565/NASA-NTV1-HLS/master.m3u8"
+  },
+  {
+    name: "Al Jazeera English",
+    category: "News",
+    logo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Aljazeera_eng.svg/200px-Aljazeera_eng.svg.png",
+    url: "https://live-hls-web-aje.getaj.net/AJE/index.m3u8"
+  },
+  {
+    name: "France 24 English",
+    category: "News",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/France_24_logo.svg/200px-France_24_logo.svg.png",
+    url: "https://stream.france24.com/hls/live/2037163/F24_EN_HI_HLS/master.m3u8"
+  },
+  {
+    name: "DW News",
+    category: "News",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Deutsche_Welle_symbol_2012.svg/200px-Deutsche_Welle_symbol_2012.svg.png",
+    url: "https://dwamdstream102.akamaized.net/hls/live/2015525/dwstream102/index.m3u8"
+  }
+];
